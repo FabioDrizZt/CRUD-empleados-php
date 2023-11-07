df2e94d780363fe9f40228265e0a9e1ede76abc0
fabio <fabio@digitalmind.com>
2023-11-06 21:59:12 -0300

    Index y creacion de empleados

--- secciones/empleados/crear.php
+++ secciones/empleados/crear.php
@@ -1,4 +1,47 @@
 <?php require_once('../../templates/head.php') ?>
 <?php include_once('../../templates/header.php') ?>
-<main class="container">Esta es la creación de empleados</main>
+<main class="container">
+  <div class="card">
+    <div class="card-header">
+      Creación de empleados
+    </div>
+    <div class="card-body">
+      <form method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+          <label for="primernombre" class="form-label">Primer Nombre</label>
+          <input type="text" class="form-control" name="primernombre" id="primernombre" aria-describedby="helpId" placeholder="">
+        </div>
+        <div class="mb-3">
+          <label for="segundonombre" class="form-label">Segundo Nombre</label>
+          <input type="text" class="form-control" name="segundonombre" id="segundonombre" aria-describedby="helpId" placeholder="">
+        </div>
+        <div class="mb-3">
+          <label for="primerapellido" class="form-label">Primer Apellido</label>
+          <input type="text" class="form-control" name="primerapellido" id="primerapellido" aria-describedby="helpId" placeholder="">
+        </div>
+        <div class="mb-3">
+          <label for="segundoapellido" class="form-label">Segundo Apellido</label>
+          <input type="text" class="form-control" name="segundoapellido" id="segundoapellido" aria-describedby="helpId" placeholder="">
+        </div>
+        <div class="mb-3">
+          <label for="foto" class="form-label">Elija su foto</label>
+          <input type="file" class="form-control" name="foto" id="foto" placeholder="" aria-describedby="fileHelpId">
+        </div>
+        <div class="mb-3">
+          <label for="cv" class="form-label">Suba su CV</label>
+          <input type="file" class="form-control" name="cv" id="cv" placeholder="" aria-describedby="fileHelpId">
+        </div>
+        <div class="mb-3">
+          <label for="puesto" class="form-label">Puesto</label>
+          <select class="form-select form-select-lg" name="puesto" id="puesto">
+            <option selected>Seleccione su puesto</option>
+            <option value="1">Backend</option>
+            <option value="2">Frontend</option>
+            <option value="3">Fullstack</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
+</main>
 <?php require_once('../../templates/footer.php') ?>
--- secciones/empleados/index.php
+++ secciones/empleados/index.php
@@ -1,4 +1,43 @@
 <?php require_once('../../templates/head.php') ?>
 <?php include_once('../../templates/header.php') ?>
-<main class="container">Este es el index de empleados</main>
+<main class="container">
+  <h1>Empleados</h1>
+  <div class="card">
+    <div class="card-header">
+      <a class="btn btn-primary" href="crear.php" role="button">Agregar registro</a>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table ">
+          <thead>
+            <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Foto</th>
+              <th scope="col">CV</th>
+              <th scope="col">Puesto</th>
+              <th scope="col">Fecha de ingreso</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="">
+              <td scope="row">Fabio D. Argañaraz A.</td>
+              <td>imagen.jpg</td>
+              <td>CV.pdf</td>
+              <td>Profesor de Des.Web.</td>
+              <td>20/12/2020</td>
+              <td>
+                <a class="btn btn-success" href="carta.php" role="button">Carta</a>
+                <a class="btn btn-info" href="editar.php" role="button">Editar</a>
+                <a class="btn btn-danger" href="#" role="button">Eliminar</a>
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+  </div>
+</main>
 <?php require_once('../../templates/footer.php') ?>
