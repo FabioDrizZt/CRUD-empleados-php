cbb3ca62272521c5d472235d956bb00fa59a0ff6
fabio <fabio@digitalmind.com>
2023-11-06 21:21:57 -0300

    Creada la arquitectura de archivos y carpetas

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require_once('./templates/head.php') ?>
 <?php include_once('./templates/header.php') ?>
 <main>
-
+  Aca iria el Main
 </main>
 <?php require_once('./templates/footer.php') ?>
