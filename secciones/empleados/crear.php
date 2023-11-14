fd7d1d12a4b7e3782769e407d9fe2732de25af87
fabio <fabio@digitalmind.com>
2023-11-13 22:07:30 -0300

    Index y el eliminado de emplados

--- secciones/empleados/crear.php
+++ secciones/empleados/crear.php
@@ -1,3 +1,4 @@
+
 <?php require_once('../../templates/head.php') ?>
 <?php include_once('../../templates/header.php') ?>
 <main class="container">
--- secciones/empleados/index.php
+++ secciones/empleados/index.php
@@ -1,3 +1,22 @@
+<?php require_once('../../bd.php');
+$sentencia = $conexion->prepare("SELECT *,
+ (SELECT nombredelpuesto FROM tbl_puestos WHERE tbl_puestos.id=tbl_empleados.idpuesto) as puesto
+ FROM `tbl_empleados`"); // Traer datos desde la BD
+$sentencia->execute(); // Ejecutar la consulta previamente preparada
+$lista_tbl_empleados = $sentencia->fetchAll(PDO::FETCH_ASSOC); // Almacenar los datos de manera asociativa
+if (isset($_GET['txtID'])) {
+  //Validamos que los datos hayan sido cargado correctamente
+  $txtID = $_GET['txtID'];
+  $sentencia = $conexion->prepare("DELETE FROM `tbl_empleados` WHERE `id`=:id"); // Traer datos desde la BD
+  $sentencia->bindParam(":id", $txtID);
+  try {
+    $sentencia->execute(); // Ejecutar la consulta previamente preparada
+    header("Location:index.php");
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+  }
+}
+?>
 <?php require_once('../../templates/head.php') ?>
 <?php include_once('../../templates/header.php') ?>
 <main class="container">
@@ -11,6 +30,7 @@
         <table class="table ">
           <thead>
             <tr>
+              <th scope="col">ID</th>
               <th scope="col">Nombre</th>
               <th scope="col">Foto</th>
               <th scope="col">CV</th>
@@ -20,18 +40,22 @@
             </tr>
           </thead>
           <tbody>
-            <tr class="">
-              <td scope="row">Fabio D. Argañaraz A.</td>
-              <td>imagen.jpg</td>
-              <td>CV.pdf</td>
-              <td>Profesor de Des.Web.</td>
-              <td>20/12/2020</td>
-              <td>
-                <a class="btn btn-success" href="carta.php" role="button">Carta</a>
-                <a class="btn btn-info" href="editar.php" role="button">Editar</a>
-                <a class="btn btn-danger" href="#" role="button">Eliminar</a>
-              </td>
-            </tr>
+            <?php foreach ($lista_tbl_empleados as $registro) { ?>
+              <tr class="">
+                <td scope="row"><?= $registro['id'] ?></td>
+                <td scope="row"><?= $registro['primernombre'] . " " . $registro['segundonombre'] . " " . $registro['primerapellido'] . " " . $registro['segundoapellido'] ?></td>
+                <td><?= $registro['foto'] ?></td>
+                <td><?= $registro['cv'] ?></td>
+                <td><?= $registro['puesto'] ?></td>
+                <td><?= $registro['fechadeingreso'] ?></td>
+                <td>
+                  <a class="btn btn-success" href="carta.php" role="button">Carta</a>
+                  <a class="btn btn-info" href="editar.php?txtID=<?= $registro['id'] ?>" role="button">Editar</a>
+                  <a class="btn btn-danger" href="index.php?txtID=<?= $registro['id'] ?>" role="button">Eliminar</a>
+                </td>
+              </tr>
+            <?php } ?>
+
           </tbody>
         </table>
       </div>
